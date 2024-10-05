b75f0391fd6c725c49633224003368e25771e7bd
Iann Molendolff <iannmolendolff@gmail.com>
2024-10-05 18:46:44 -0300

    Navbar

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -1,15 +1,10 @@
 import React from 'react'
-import Image from 'next/image';
 
 const Home = () => {
   return (
     <div> 
-      <Image 
-      src="./assets/livroheader.svg"
-      width={40}
-      height={45}
-      alt='Livro'
-      />
+      
+      
       <div className="flex font-bold p-2 text-black ">
         Bem vindo
       </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import NavBar from "@/Components/Navbar";
 
 
 export const metadata: Metadata = {
@@ -13,9 +14,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body>
-        
+        <NavBar />
         {children}
       </body>
     </html>
