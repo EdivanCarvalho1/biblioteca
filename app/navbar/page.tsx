4f97828ab68c9f0ffe5af345817011d4531cc0f1
edivan junior <edivanjuniorfreitas@gmail.com>
2024-10-05 18:51:28 -0300

    m

---
+++ app/navbar/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NavBar() {
+    return (
+        <div className="bg-green-900 text-white">
+            a
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import NavBar from "./navbar/page";
 
 export default function Home() {
   return (
     <>
-
+      <NavBar></NavBar>
     </>
   );
 }
