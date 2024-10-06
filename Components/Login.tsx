5cb06b10a8377a6a31ae5144ed27f5f636ee2662
Iann Molendolff <iannmolendolff@gmail.com>
2024-10-05 23:46:34 -0300

    Pagina de Login

--- Components/Login.tsx
+++ Components/Login.tsx
@@ -10,24 +10,34 @@ const Login = () => {
       height={50}
       alt={''} />
       
-      <div className="w-full bg-green-800 rounded-lg 
-      shadow border sm:max-w-md  border-green-500 ">
-        <div className='p-3 md:space-y-6 sm:p-8'>
-          <h1 className="font-bold text-black">
+      <div className="w-full bg-green-800 rounded-lg xl:p-0
+      shadow border sm:max-w-md  border-green-500   ">
+        <div className='flex flex-col md:space-y-6 sm:p-8 '>
+          <h1 className="font-bold  text-xl md:text-2xl">
           Login
           </h1>
-          <form className='flex flex-col'>
-            <div>
-              <label htmlFor="email" className="flex"
+          <form className=' space-y-4 md:space-y-6'>
+            <div >
+              <label htmlFor="email" className="block p-1"
               >Email</label>
-              <input type="text" className='rounded-lg ' placeholder='emailexemplos@gmial.com'/>
+              <input type="text" className='rounded-lg w-full  text-black border border-green-500 bg-green-600 placeholder:text-slate-200 placeholder:p-1 p-1.5' placeholder='emailexemplos@gmail.com'/>
             </div>
 
             <div>
-              <label htmlFor="password">
+              <label htmlFor="password" className='block p-1'>
                 Senha
               </label>
+              <input type="password"  className="rounded-lg border font-semibold  border-green-500 text-black bg-green-600 placeholder:p-1 w-full p-1.5" />
             </div>
+            <div>
+              
+            </div>
+            <a href="" className=' flex flex-row-reverse  hover:underline'>Esqueci a senha</a>
+            <button type='submit' className='hover:bg-green-600 bg-green-700 rounded-xl w-full p-1'>
+              Entrar
+            </button>
+
+            <p className='text-sm font-light text-green-200 flex '>Ainda não possui uma conta? <a href="#" className='hover:underlineflex '>Crie aqui</a></p>
           </form>
         </div>
       </div>
